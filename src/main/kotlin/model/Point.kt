42a87f6af7c03fa873eb70eb7d16db584352f621
Devan Kavalchek <devankav7@gmail.com>
2021-10-03 18:11:47 -0400

    initial commit

---
+++ src/main/kotlin/model/Point.kt
@@ -0,0 +1,14 @@
+package model
+
+class Point<E>(val x: Int, val y: Int, val value: E?) {
+    infix fun dot(other: Point<E>): Int {
+        return (this.x * other.x) + (this.y * other.y)
+    }
+}
+
+fun main() {
+    val a = Point<Int>(1, 2)
+    val b = Point<Int>(2, 3)
+
+    println(a dot b)
+}
